96c0d752919e69ce7ffe1fee897281e198880167
kolbinglebvadimovich <sonya54842@yandex.ru>
2026-02-23 17:17:03 +0700

    Update main.cpp

--- С++.reshenie/main.cpp
+++ С++.reshenie/main.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 
-void printHello() // Обьевление (прототип) функции.
-
-{
-
-    std::cout << "Bye bye Gleb!!!" << std::endl;
-
-}
+void printHello();
+double sum(double first, double second);
+void printName(std::strig name);
 
 int main()
 {
-
     printHello();
+    double variable1 { 110.0 };
+    double variable2 { 55.1 };
+    std::cout << "Sim :" << sum(variable1 , variable2) << std::endl;
+
+    printName ("Gleb");
 
     return EXIT_SUCCESS;
-}   
+
+}
+
+void printHello()
+{
+    if ( true )
+    {
+        return;
+    }
+
+    std::cout << "Hello Gleb!!!" << std::endl;
+
+}
+    
+double sum(double first, double second);
+{
+    return first + second;
+}
+
+void printName (std::string name)
+{
+    std::cout << "Hello, " << name << std::endl;
+}
+
 
 
 
