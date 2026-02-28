b4d1a0922f3d69a5ff2c260ecc7e4349db14dad4
kolbinglebvadimovich <sonya54842@yandex.ru>
2026-02-28 12:49:35 +0700

    Update main.cpp

--- С++.reshenie/main.cpp
+++ С++.reshenie/main.cpp
@@ -1,42 +1,49 @@
 #include <iostream>
 
-void printHello();
-double sum(double first, double second);
-void printName(std::strig name);
-
-int main()
+int main(int argc, char** argv)
 {
-    printHello();
-    double variable1 { 110.0 };
-    double variable2 { 55.1 };
-    std::cout << "Sim :" << sum(variable1 , variable2) << std::endl;
+    int a = 1, b = 5;
 
-    printName ("Gleb");
+    int s = sum( a , b );
+    int dif = diff( a , b );
+    int mult = multiplication( a , b );
+    double div = division( a , b );
 
-    return EXIT_SUCCESS;
 
+    std::cout << a << " + " << b << " = " << s << std::endl;
+    std::cout << a << " - " << b << " = " << dif << std::endl;
+    std::cout << a << " * " << b << " = " << mult << std::endl;
+    std::cout << a << " / " << b << " = " << div << std::endl;
+
+
+    return 0;
 }
 
-void printHello()
+int main(int argc, char** argv)
 {
-    if ( true )
+
+    int value = 5, power = 2, result = 1; // Значение раз.
+    value = 3, power = 2, result = 1; // Значение два.
+    value = 4, power = 3, result = 1; // Значение три.
+    for(int i = 0, i < power; i++)
     {
-        return;
+        result *= value;
     }
+    std::cout << value << " в степени " << power << " = " << result << std::endl; // Вывведение в консоль.
 
-    std::cout << "Hello Gleb!!!" << std::endl;
 
+    return 0;
 }
-    
-double sum(double first, double second);
+int main(int argc, char** argv)
 {
-    return first + second;
+    std::cout << "Введитe число" << std::endl;
+    for(int a = 0, b = 1, e = 2, r = 3, g = 5.)
+    std::cout << "Введитe число" << std::endl;
+    std::cin << variable << std::endl;
+    std::cout << variable << std::endl;
 }
 
-void printName (std::string name)
-{
-    std::cout << "Hello, " << name << std::endl;
-}
+
 
 
 
