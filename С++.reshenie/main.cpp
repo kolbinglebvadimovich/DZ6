277352dc99003b0bb41b60b3837f3d2a5dfa11c2
kolbinglebvadimovich <sonya54842@yandex.ru>
2026-04-06 20:21:55 +0700

    Update main.cpp

--- С++.reshenie/main.cpp
+++ С++.reshenie/main.cpp
@@ -4,10 +4,10 @@ int main(int argc, char** argv)
 {
     int a = 5, b = 10;
 
-    int s = sum( a , b );
-    int dif = diff( a , b );
-    int mult = multiplication( a , b );
-    double div = division( a , b );
+    int sum(int x, int y) { return x + y; }
+    int diff(int x, int y) { return x - y; }
+    int multiplication(int x, int y) { return x * y; }
+    double division(int x, int y) { return y != 0 ? double(x)/y : 0; }
 
 
     std::cout << a << " + " << b << " = " << s << std::endl;
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
     std::cout << a << " / " << b << " = " << div << std::endl;
 
 
-    return 0;
+   
 }
 
 
@@ -33,28 +33,24 @@ void calculateAndPrintPower(int value, int power)
     calculateAndPrintPower(5, 2);
     calculateAndPrintPower(3, 3);
     calculateAndPrintPower(4, 4);
-
-    return 0;
 }
 
 
 {
-    unsigned long long fibonacci(unsigned n)
-    
-    if (n == 0)
-        return 0;
-    if (n == 1)
-        return 1;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    unsigned long long fibonacci(unsigned n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
 }
 {
 
 
-    std::cout << "Введите количество чисел последовательности Фибоначчи: "; << std::endl;
-    std::cin >> count >>
+    std::cout << "Введите количество чисел последовательности Фибоначчи: " << std::endl;
+    std::cin >> count;
     {
     std::cout << "Последовательность Фибоначчи (" << count << " чисел):" << std::endl;
-
+        for(int i = 0; i < count; i++)
+            
         std::cout << fibonacci(i) << " " << std::endl;
     }
 
